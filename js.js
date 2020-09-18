4c3fd930b07838bf9c6b7af8f4ee108e5a5dbdf9
plewekat <59675811+plewekat@users.noreply.github.com>
2020-09-18 00:17:07 -0700

    1

--- js.js
+++ js.js
@@ -1,13 +1,2 @@
-try {
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-  };
-  document.getElementById(
-  "currentdate"
-).textContent = new Date().toLocaleDateString("en-US", options);
-  } catch (e) {
-    alert("error with code");
-  }
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString();
