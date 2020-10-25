6debee96c9da0571634d86127c97fae17471b2d3
plewekat <59675811+plewekat@users.noreply.github.com>
2020-10-25 02:12:35 -0700

    2

--- lesson5/js/lesson5.js
+++ lesson5/js/lesson5.js
@@ -15,23 +15,3 @@ const dateFormatOptions = {
 };
 
 currentDateElement.textContent = currentDate.toLocaleDateString('en-US', dateFormatOptions);
-
-
-var btnContainer = document.getElementById("flex-container");
-
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-
-    // If there's no active class
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-
-    // Add the active class to the current/clicked button
-    this.className += " active";
-  });
-}
