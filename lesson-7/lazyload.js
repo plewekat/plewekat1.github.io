b09988d8e3737f52e67c09e5a290c9048b801a84
plewekat <59675811+plewekat@users.noreply.github.com>
2020-11-20 14:14:12 -0800

    5

--- lesson-7/lazyload.js
+++ lesson-7/lazyload.js
@@ -1,4 +1,4 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
   image.onload = () => {image.removeAttribute("data-src");
