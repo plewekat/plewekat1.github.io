26c77696e3e2ade5ff99d49a1b832c9a52aee55c
plewekat <59675811+plewekat@users.noreply.github.com>
2020-12-16 22:59:59 -0800

    6

--- Scoots/lesson-11/index.js
+++ Scoots/lesson-11/index.js
@@ -9,37 +9,69 @@ fetch(requestURL)
   const towns = jsonObject['towns'];
   
    for (let i = 0; i < towns.length; i++) {
-     if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs" ){ 
+     if (towns[i].name == "Preston"){ 
    let card = document.createElement('section');
    let h2 = document.createElement('h2');
-   let h3 = document.createElement('h3');
-   let yearFounded = document.createElement('p');
-   let population = document.createElement('p');   
-   let rainfall = document.createElement('p');
-   let image = document.createElement('img')
      
-     h2.textContent = towns[i].name;
-     h3.textContent = towns[i].motto;
-     yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
-     population.textContent = "Population: " + towns[i].currentPopulation;
-     rainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
-     image.setAttribute('src', "https://plewekat.github.io/images/" + towns[i].photo);
-     image.setAttribute('alt', towns[i].name)
+     h2.textContent = towns[i].events;
      
      card.appendChild(h2);
-     card.appendChild(h3);
-     card.appendChild(yearFounded);
-     card.appendChild(population);
-     card.appendChild(rainfall);
-     card.appendChild(image);
    
    document.querySelector('div.cards').appendChild(card);
 }
 }    
 }); 
 
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=20.5083&lon=-86.9458&units=imperial&APPID=b84b59a1755e37713619b7756a56bd66";
+
+fetch(weatherURL)
+  .then(response => response.json())
+  .then(jsObject => {
+
+    const curweather = document.getElementById('current-weather');
+    const curtemp = document.getElementById('current-temp');
+    const curwindspeed = document.getElementById('current-wind-speed');
+    const curhumidity = document.getElementById('current-humidity');
+
+    curhumidity.innerText = jsObject.main.humidity;
+    curweather.innerText = jsObject.weather[0].main;
+    curtemp.innerText = (jsObject.main.temp).toFixed(0) + " ℉";
+    curwindspeed.innerText = (jsObject.wind.speed).toFixed(0) + "mph";
+ 
+    const temperature = parseFloat(jsObject.main.temp);
+    const windspeed = parseFloat(jsObject.wind.speed);
+    let windchill = "N/A";
+    if (temperature <= 50 && windspeed >= 3) {
+      windchill = (35.74+0.6215*temperature-35.75*Math.pow(windspeed, 0.16)+0.4275*temperature*Math.pow(windspeed, 0.16)).toFixed(0);
+      windchill = windchill + "℉"
+    }
+    document.getElementById('current-wind-chill').textContent = windchill;
+});
+
+
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=20.5083&lon=-86.9458&units=imperial&APPID=b84b59a1755e37713619b7756a56bd66";
+
+
+fetch(forecastURL)
+  .then(response => response.json())
+  .then(jsObject => {
+  
+    const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+    for (let day = 0; day < forecast.length; day++) {
+      const today = forecast[day];
+      const date = new Date(today.dt_txt);
+      const imgsource = `https://openweathermap.org/img/w/${today.weather[0].icon}.png`;
+      document.getElementById(`dayofweek${day+1}`).textContent = weekdays[date.getDay()];
+      document.getElementById(`forecast${day+1}`).textContent = (today.main.temp_max).toFixed(0);
+      document.getElementById((`imagesrc${day+1}`)).setAttribute("src", imgsource);
+      document.getElementById((`imagesrc${day+1}`)).setAttribute("alt", date);
+    }
+  });
+
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.flex-container')
+const mainnav = document.querySelector('.flex-container');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
@@ -70,46 +102,40 @@ document.getElementById('alertMsg').classList.add('alertMessage');
 document.getElementById('alertMsg').classList.remove('alertMessageYes');
 };
 
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {image.removeAttribute("data-src");
-  };
-};
 
 const imgOptions = {
-threshold: 1,
-rootMargin: "0px 0px 50px 0px"
-};
-
-if ("IntersectionObserver" in window) {
-const observer = new IntersectionObserver((items, observer) => {
-  items.forEach((item) => {
-  if(item.isIntersecting) {
-    loadImages(item.target);
-    observer.unobserve(item.target);
-  }
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+  };
+  
+  if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+    if(item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+    });
   });
-});
-
-imagesToLoad.forEach((img) => {
-observer.observe(img)
-});                     
-} else {
+  
   imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
-}
-
-function myFunction(elmnt,clr) {
-  elmnt.style.color = clr;
-}
-
-WebFont.load({
-  google: {
-    families: [
-       'Abel', 'Roboto', 'nanum gothic'
-    ]
+  observer.observe(img)
+  });                     
+  } else {
+    imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
   }
   
-});
+  function myFunction(elmnt,clr) {
+    elmnt.style.color = clr;
+  }
+  
+  WebFont.load({
+    google: {
+      families: [
+         'heebo', 'Roboto', 'manrope'
+      ]
+    }
+    
+  });
